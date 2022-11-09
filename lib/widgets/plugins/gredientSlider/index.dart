39c4997b7e00c57f619c65ba0cf6f25a54e15044
lirh42 <lirh42@midea.com>
2022-11-09 15:29:02 +0800

    feat: 滑动条触控

--- lib/widgets/plugins/gredientSlider/index.dart
+++ lib/widgets/plugins/gredientSlider/index.dart
@@ -20,7 +20,8 @@ class GredientSlider extends StatefulWidget {
   // 插槽
   // final Widget child;
 
-  GredientSlider({super.key,
+  GredientSlider({
+    super.key,
     required this.value,
     this.activeColors = const [Color(0xFF267AFF), Color(0xFF267AFF)],
     this.max = 100,
@@ -35,76 +36,88 @@ class GredientSlider extends StatefulWidget {
 }
 
 class _GredientSliderState extends State<GredientSlider> {
-
   _GredientSliderState(double value);
 
   @override
   Widget build(BuildContext context) {
-    double leftValue = widget.value / 100 * 258;
+    double leftValue = widget.value / 100 * widget.width - 12 - 4 + 20;
 
-    return ConstrainedBox(
-      constraints:
-          BoxConstraints.tightFor(height: widget.height, width: widget.width),
-      child: Stack(
-        children: <Widget>[
-          DecoratedBox(
-            decoration: BoxDecoration(
-              color: const Color(0xFF1F1F1F),
-              borderRadius: widget.radius
-                  ? BorderRadius.circular(10.0)
-                  : BorderRadius.circular(0),
+    return GestureDetector(
+        onPanDown: (DragDownDetails e) {
+          //打印手指按下的位置(相对于屏幕)
+          setState(() {
+            double temp = (e.localPosition.dx - 20 - 10) / (widget.width - 20) * 100;
+            if (temp > 100) {
+              temp = 100;
+            } else if (temp < 0) {
+              temp = 0;
+            }
+            setState(() {
+              widget.value = temp;
+            });
+          });
+        },
+        //手指滑动时会触发此回调
+        onPanUpdate: (DragUpdateDetails e) {
+          //用户手指滑动时，更新偏移，重新构建
+          double temp = (e.localPosition.dx - 20 - 10) / (widget.width - 20) * 100;
+          if (temp > 100) {
+            temp = 100;
+          } else if (temp < 0) {
+            temp = 0;
+          }
+          setState(() {
+            widget.value = temp;
+          });
+        },
+        onPanEnd: (DragEndDetails e) {
+          //打印滑动结束时在x、y轴上的速度
+        },
+        child: Container(
+            padding: const EdgeInsets.all(20.0),
+            decoration: const BoxDecoration(
+              color: Colors.transparent
             ),
             child: ConstrainedBox(
-                constraints: BoxConstraints.tightFor(
-                    height: widget.height, width: widget.width)),
-          ),
-          GestureDetector(
-              onPanDown: (DragDownDetails e) {
-                //打印手指按下的位置(相对于屏幕)
-                print("用户手指按下：${e.localPosition}");
-              },
-              //手指滑动时会触发此回调
-              onPanUpdate: (DragUpdateDetails e) {
-                //用户手指滑动时，更新偏移，重新构建
-                setState(() {
-                  double dir = e.delta.dx > 0 ? 1 : -1;
-                  double dan = ((e.delta.dx / widget.width).abs() > 1 ? (e.delta.dx / widget.width) : 2);
-                  if (widget.value + dir * dan > 100) {
-                    widget.value = 100;
-                  } else if (widget.value + dir * dan < 0) {
-                    widget.value = 0;
-                  } else {
-                    widget.value += dir * dan;
-                  }});
-                print("用户手指滑动：${e.delta.dx}");
-              },
-              onPanEnd: (DragEndDetails e) {
-                //打印滑动结束时在x、y轴上的速度
-                print(e.velocity);
-              },
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: widget.activeColors),
-                  borderRadius: widget.radius
-                      ? BorderRadius.circular(10.0)
-                      : BorderRadius.circular(0),
-                ),
-                child: ConstrainedBox(
-                    constraints: BoxConstraints.tightFor(
-                        height: widget.height, width: widget.width)),
-              )),
-          Positioned(
-              left: widget.value / 100 * 258,
-              top: 4,
-              child: Container(
-                width: 12,
-                height: 12,
-                decoration: BoxDecoration(
-                    color: const Color(0xFFFFFFFF),
-                    borderRadius: BorderRadius.circular(10.0)),
-              )),
-        ],
-      ),
-    );
+              constraints: BoxConstraints.tightFor(
+                  height: widget.height, width: widget.width),
+              child: Stack(
+                children: <Widget>[
+                  DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF1F1F1F),
+                      borderRadius: widget.radius
+                          ? BorderRadius.circular(10.0)
+                          : BorderRadius.circular(0),
+                    ),
+                    child: ConstrainedBox(
+                        constraints: BoxConstraints.tightFor(
+                            height: widget.height, width: widget.width)),
+                  ),
+                  DecoratedBox(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: widget.activeColors),
+                      borderRadius: widget.radius
+                          ? BorderRadius.circular(10.0)
+                          : BorderRadius.circular(0),
+                    ),
+                    child: ConstrainedBox(
+                        constraints: BoxConstraints.tightFor(
+                            height: widget.height,
+                            width: 20 + widget.value / 100 * (widget.width - 20))),
+                  ),
+                  Positioned(
+                      left: 20 - 16 + widget.value / 100 * (widget.width - 20),
+                      top: 4,
+                      child: Container(
+                        width: 12,
+                        height: 12,
+                        decoration: BoxDecoration(
+                            color: const Color(0xFFFFFFFF),
+                            borderRadius: BorderRadius.circular(10.0)),
+                      )),
+                ],
+              ),
+            )));
   }
 }
