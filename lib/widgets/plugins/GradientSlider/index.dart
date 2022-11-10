4c36510cca6586d573c8f9ae4fbf6c3adf195d7f
lirh42 <lirh42@midea.com>
2022-11-10 17:17:15 +0800

    feat: 合并

--- lib/widgets/plugins/GradientSlider/index.dart
+++ lib/widgets/plugins/GradientSlider/index.dart
@@ -33,7 +33,7 @@ class GradientSlider extends StatefulWidget {
     this.activeColors = const [Color(0xFF267AFF), Color(0xFF267AFF)],
     this.max = 100,
     this.min = 0,
-    this.width = 258,
+    this.width = 272,
     this.height = 20,
     this.radius = true,
   });
@@ -77,59 +77,94 @@ class _GradientSliderState extends State<GradientSlider>
       onPanUpdate: (e) => onPanUpdate(e),
       onPanEnd: (e) => onPanUp(),
       onPanCancel: () => onPanUp(),
-      child: Container(
-        padding: const EdgeInsets.all(20.0),
-        decoration: const BoxDecoration(color: Colors.transparent),
-        child: ConstrainedBox(
-          constraints: BoxConstraints.tightFor(
-            height: widget.height,
-            width: widget.width,
+      child: Stack(
+        alignment: Alignment.centerLeft,
+        children: [
+          Container(
+            padding: const EdgeInsets.fromLTRB(0, 10, 0, 22),
+            decoration: const BoxDecoration(color: Colors.transparent),
+            child: ConstrainedBox(
+              constraints: BoxConstraints.tightFor(
+                height: widget.height,
+                width: widget.width,
+              ),
+              child: Stack(
+                alignment: Alignment.centerLeft, //指定未定位或部分定位widget的对齐方式
+                children: <Widget>[
+                  DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF1F1F1F),
+                      borderRadius: widget.radius
+                          ? BorderRadius.circular(10.0)
+                          : BorderRadius.circular(0),
+                    ),
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints.tightFor(
+                        height: widget.height,
+                        width: widget.width,
+                      ),
+                    ),
+                  ),
+                  DecoratedBox(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: widget.activeColors),
+                      borderRadius: widget.radius
+                          ? BorderRadius.circular(10.0)
+                          : BorderRadius.circular(0),
+                    ),
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints.tightFor(
+                        height: widget.height,
+                        width: 20 + value / 100 * (widget.width - 20),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 20 - 16 + value / 100 * (widget.width - 20),
+                    child: Container(
+                      width: 12,
+                      height: 12,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFFFFFFF),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-          child: Stack(
-            alignment: Alignment.centerLeft, //指定未定位或部分定位widget的对齐方式
-            children: <Widget>[
-              DecoratedBox(
-                decoration: BoxDecoration(
-                  color: const Color(0xFF1F1F1F),
-                  borderRadius: widget.radius
-                      ? BorderRadius.circular(10.0)
-                      : BorderRadius.circular(0),
-                ),
-                child: ConstrainedBox(
-                  constraints: BoxConstraints.tightFor(
-                    height: widget.height,
-                    width: widget.width,
-                  ),
-                ),
+          const Positioned(
+            left: 0,
+            bottom: 0,
+            child: Text(
+              '0',
+              style: TextStyle(
+                fontFamily: "MideaType",
+                fontSize: 12,
+                height: 1,
+                color: Color(0x80FFFFFF),
+                fontWeight: FontWeight.w300,
+                decoration: TextDecoration.none,
               ),
-              DecoratedBox(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: widget.activeColors),
-                  borderRadius: widget.radius
-                      ? BorderRadius.circular(10.0)
-                      : BorderRadius.circular(0),
-                ),
-                child: ConstrainedBox(
-                  constraints: BoxConstraints.tightFor(
-                    height: widget.height,
-                    width: 20 + value / 100 * (widget.width - 20),
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 20 - 16 + value / 100 * (widget.width - 20),
-                child: Container(
-                  width: 12,
-                  height: 12,
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFFFFFFF),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
+          const Positioned(
+            right: 0,
+            bottom: 0,
+            child: Text(
+              '100%',
+              style: TextStyle(
+                fontFamily: "MideaType",
+                fontSize: 12,
+                height: 1,
+                color: Color(0x80FFFFFF),
+                fontWeight: FontWeight.w300,
+                decoration: TextDecoration.none,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/widgets/plugins/ModeCard/index.dart
+++ lib/widgets/plugins/ModeCard/index.dart
@@ -1,2 +1,43 @@
+import 'dart:core';
 import 'package:flutter/material.dart';
+import 'package:screen_app/widgets/plugins/GlassCard/index.dart';
 
+class Mode {
+  String index;
+  String name;
+  bool selected;
+
+  Mode(this.index, this.name, this.selected);
+
+  void select() {
+    selected = true;
+  }
+}
+
+class ModeCard extends StatefulWidget {
+
+  const ModeCard({
+    super.key,
+  });
+
+  @override
+  State<ModeCard> createState() => _ModeCardState();
+}
+
+class _ModeCardState extends State<ModeCard> {
+  late List<Mode> modeList;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return GlassCard(child: )
+
+    throw UnimplementedError();
+  }
+
+}
