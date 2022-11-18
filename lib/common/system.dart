d315f8313d3b74c1bb8e12202670fe3ee60952d9
kevinsheep <yangws2@midea.com>
2022-11-18 14:29:01 +0800

    prof: 天气背景预加载优化

---
+++ lib/common/system.dart
@@ -0,0 +1,24 @@
+// 等待多久进入待机状态
+List _standbyTime = [
+  {'value': 30, 'text': '30秒'},
+  {'value': 60, 'text': '1分钟'},
+  {'value': 120, 'text': '3分钟'},
+  {'value': 300, 'text': '5分钟'},
+  {'value': -1, 'text': '永不'},
+];
+
+Map weatherData = {
+  'standbyTime': _standbyTime.first['value'],
+  'weatherCode': '',
+};
+
+// 待机设置
+class StandbySetting {
+  static get standbyTime => weatherData['standbyTime'];
+
+  static String get weatherCode => weatherData['weatherCode'];
+
+  static set weatherCode(String code) {
+    weatherData['weatherCode'] = code;
+  }
+}
