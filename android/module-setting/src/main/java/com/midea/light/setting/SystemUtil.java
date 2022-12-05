f8a3b06eaa89b9799330a000ccdcad0ef0b78ba7
weinp <effective_code@163.com>
2022-12-05 17:57:33 +0800

    [Android][功能]封装SystemUtil工具类。区分灵动与晶华

---
+++ android/module-setting/src/main/java/com/midea/light/setting/SystemUtil.java
@@ -0,0 +1,575 @@
+package com.midea.light.setting;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.SmatekManager;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.PowerManager;
+import android.os.SystemClock;
+import android.provider.Settings;
+import android.view.View;
+
+import com.jhxs.ltmidea.tools.RelayControl;
+import com.midea.light.BaseApplication;
+import com.midea.light.common.config.AppCommonConfig;
+import com.midea.light.utils.CommandExecution;
+
+import java.lang.reflect.InvocationTargetException;
+
+class JHSystemUtil {
+
+    static {
+        System.loadLibrary("ltmidea");
+    }
+
+    /**
+     * 移除所有的数据
+     */
+    @SuppressLint("WrongConstant")
+    public static void eraseAllData() {
+        throw new RuntimeException("暂不支持此操作");
+    }
+
+    public static void setScreenAutoMode(boolean isAutoMode) {
+        ContentResolver contentResolver = BaseApplication.getContext().getContentResolver();
+        if (isAutoMode) {
+            Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC);
+        } else {
+            Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+        }
+    }
+
+    public static boolean isScreenAutoMode() {
+        boolean isAutoMode = false;
+        ContentResolver contentResolver = BaseApplication.getContext().getContentResolver();
+        try {
+            int mode = Settings.System.getInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS_MODE);
+            if (mode == Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC) {
+                isAutoMode = true;
+            } else {
+                isAutoMode = false;
+            }
+        } catch (Settings.SettingNotFoundException e) {
+            e.printStackTrace();
+        }
+        return isAutoMode;
+    }
+
+    /**
+     * 重启
+     */
+    @SuppressLint("WrongConstant")
+    public static void reboot() {
+        String command = "setprop sys.powerctl reboot";
+        CommandExecution.execCommand(command, false);
+    }
+
+    /**
+     * 关机
+     */
+    @SuppressLint("WrongConstant")
+    public static void shutdown() {
+        String command = "setprop sys.powerctl shutdown";
+        CommandExecution.execCommand(command, false);
+    }
+
+    //断开以太网
+    @SuppressLint("WrongConstant")
+    public static void disconnectEth() {
+        throw new RuntimeException("暂不支持此操作");
+    }
+
+    //连接以太网
+    @SuppressLint("WrongConstant")
+    public static void connectEth() {
+        throw new RuntimeException("暂不支持此操作");
+    }
+
+    /**
+     * 调节亮度
+     * 亮度值（0~255）
+     */
+    @SuppressLint("WrongConstant")
+    public static void lightSet(int light) {
+        Settings.System.putInt(BaseApplication.getContext().getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, light);
+    }
+
+    /**
+     * 开关背光
+     *
+     * @param value
+     */
+    @SuppressLint("WrongConstant")
+    public static void setLcdBlackLight(boolean value) {
+        PowerManager powerManager = (PowerManager) BaseApplication.getContext().getSystemService(Context.POWER_SERVICE);
+        if (value) {
+            try {
+                if (powerManager != null) {
+                    powerManager.getClass().getMethod("wakeUp", long.class).invoke(powerManager, SystemClock.uptimeMillis());
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                powerManager.getClass().getMethod("goToSleep", new Class[]{long.class}).invoke(powerManager, SystemClock.uptimeMillis());
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    /**
+     * 开关背光
+     *
+     * @param value
+     */
+    @SuppressLint("WrongConstant")
+    public static void openOrCloseScreen(boolean value) {
+        PowerManager powerManager = (PowerManager) BaseApplication.getContext().getSystemService(Context.POWER_SERVICE);
+        if (!value) {
+            try {
+                if (powerManager != null) {
+                    powerManager.getClass().getMethod("wakeUp", long.class).invoke(powerManager, SystemClock.uptimeMillis());
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                powerManager.getClass().getMethod("goToSleep", new Class[]{long.class}).invoke(powerManager, SystemClock.uptimeMillis());
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 隐藏导航栏
+     */
+    public static void hideNavigationBar(Activity activity) {
+        activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
+
+    /**
+     * 显示导航栏
+     *
+     * @param context
+     */
+    public static void showNavigationBar(Context context) {
+        throw new RuntimeException("暂不支持此方法调用");
+    }
+
+    /**
+     * 继电器控制
+     */
+    @SuppressLint("WrongConstant")
+    public static void CommandGP(int num, boolean onoff) {
+        try {
+            switch (num) {
+                case 0:
+                    RelayControl.SetRelayGpio1Status(onoff ? 1 : 0);
+                    break;
+                case 1:
+                    RelayControl.SetRelayGpio2Status(onoff ? 1 : 0);
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 继电器控制
+     */
+    @SuppressLint("WrongConstant")
+    public static Object readGP(int num) {
+        try {
+            switch (num) {
+                case 0:
+                    return String.valueOf(RelayControl.GetRelayGpio1Status());
+                case 1:
+                    return String.valueOf(RelayControl.GetRelayGpio2Status());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return "0";
+    }
+
+    public static void clickScreen(int x,int y){
+        String command = "input tap "+x+" "+y;
+        new Thread(){
+            public void run(){
+                CommandExecution.execCommand(command, false);
+            }
+        }.start();
+
+    }
+
+    public static void wakeupScreenAndClickScreen(int x,int y){
+        clickScreen(x,y);
+    }
+
+}
+
+class LDSystemUtil {
+    /**
+     * 移除所有的数据
+     */
+    @SuppressLint("WrongConstant")
+    public static void eraseAllData() {
+        SmatekManager manager = (SmatekManager) BaseApplication.getContext().getSystemService("smatek");
+        manager.eraseAllData();
+    }
+
+    public static void setScreenAutoMode(boolean isAutoMode) {
+        throw new RuntimeException("暂不支持此操作");
+    }
+
+    public static boolean isScreenAutoMode() {
+        throw new RuntimeException("暂不支持此操作");
+    }
+
+    /**
+     * 重启
+     */
+    @SuppressLint("WrongConstant")
+    public static void reboot() {
+        SmatekManager manager = (SmatekManager) BaseApplication.getContext().getSystemService("smatek");
+        manager.reboot();
+    }
+
+    /**
+     * 关机
+     */
+    @SuppressLint("WrongConstant")
+    public static void shutdown() {
+        SmatekManager manager = (SmatekManager) BaseApplication.getContext().getSystemService("smatek");
+        manager.shutdown();
+    }
+
+    //断开以太网
+    @SuppressLint("WrongConstant")
+    public static void disconnectEth() {
+        SmatekManager manager = (SmatekManager) BaseApplication.getContext().getSystemService("smatek");
+        manager.disconnectEth("eth0");
+    }
+
+    //连接以太网
+    @SuppressLint("WrongConstant")
+    public static void connectEth() {
+        SmatekManager manager = (SmatekManager) BaseApplication.getContext().getSystemService("smatek");
+        manager.connectEth("eth0");
+    }
+
+    /**
+     * 调节亮度
+     * 亮度值（0~255）
+     */
+    @SuppressLint("WrongConstant")
+    public static void lightSet(int light) {
+        SmatekManager manager = (SmatekManager) BaseApplication.getContext().getSystemService("smatek");
+        manager.setBrightness(light);
+    }
+
+    /**
+     * 开关背光
+     *
+     * @param value
+     */
+    @SuppressLint("WrongConstant")
+    public static void setLcdBlackLight(boolean value) {
+        SmatekManager manager = (SmatekManager) BaseApplication.getContext().getSystemService("smatek");
+        manager.setLcdBlackLight(value);
+    }
+
+    /**
+     * 开关背光
+     *
+     * @param value
+     */
+    @SuppressLint("WrongConstant")
+    public static void openOrCloseScreen(boolean value) {
+        SmatekManager manager = (SmatekManager) BaseApplication.getContext().getSystemService("smatek");
+        manager.openOrCloseScreen(value);
+    }
+
+    /**
+     * 隐藏导航栏
+     */
+    public static void hideNavigationBar(Activity activity) {
+        Intent intent = new Intent();
+        intent.setAction("com.smatek.show.navigationbar");
+        intent.putExtra("show_navigationbar", false);//auto true 为显⽰ ，false 为 隐
+        activity.sendBroadcast(intent);
+    }
+
+    /**
+     * 显示导航栏
+     *
+     * @param context
+     */
+    public static void showNavigationBar(Context context) {
+        Intent intent = new Intent();
+        intent.setAction("com.smatek.show.navigationbar");
+        intent.putExtra("show_navigationbar", true);//auto true 为显⽰ ，false 为 隐
+        context.sendBroadcast(intent);
+    }
+
+    /**
+     * 继电器控制
+     */
+    @SuppressLint("WrongConstant")
+    public static void CommandGP(int num, boolean onoff) {
+        SmatekManager manager = (SmatekManager) BaseApplication.getContext().getSystemService("smatek");
+        switch (num) {
+            case 0:
+                if (onoff) {
+                    manager.writeToNode("sys/class/gpio/gpio114/value", "1");
+                } else {
+                    manager.writeToNode("sys/class/gpio/gpio114/value", "0");
+                }
+                break;
+            case 1:
+                if (onoff) {
+                    manager.writeToNode("/sys/class/gpio/gpio115/value", "1");
+                } else {
+                    manager.writeToNode( "/sys/class/gpio/gpio115/value", "0");
+                }
+                break;
+        }
+    }
+
+    /**
+     * 继电器控制
+     */
+    @SuppressLint("WrongConstant")
+    public static Object readGP(int num) {
+        SmatekManager manager = (SmatekManager) BaseApplication.getContext().getSystemService("smatek");
+        switch (num) {
+            case 0:
+                return manager.getDataFromNode("sys/class/gpio/gpio114/value");
+            case 1:
+                return manager.getDataFromNode("/sys/class/gpio/gpio115/value");
+        }
+        return false;
+    }
+
+    public static void clickScreen(int x,int y) {
+        String command = "input tap "+ x +" "+ y;
+        new Thread() {
+            public void run() {
+                CommandExecution.execCommand(command, false);
+            }
+        }.start();
+
+    }
+
+    public static void wakeupScreenAndClickScreen(int x,int y) {
+        clickScreen(x,y);
+    }
+}
+
+public class SystemUtil {
+    /**
+     * 移除所有的数据
+     */
+    @SuppressLint("WrongConstant")
+    public static void eraseAllData() {
+        if(AppCommonConfig.getChannel().equals("JH")){
+            JHSystemUtil.eraseAllData();
+        } else {
+            LDSystemUtil.eraseAllData();
+        }
+    }
+
+    public static void setScreenAutoMode(boolean isAutoMode) {
+        if(AppCommonConfig.getChannel().equals("JH")){
+            JHSystemUtil.setScreenAutoMode(isAutoMode);
+        } else {
+            LDSystemUtil.setScreenAutoMode(isAutoMode);
+        }
+    }
+
+    public static boolean isScreenAutoMode() {
+        if(AppCommonConfig.getChannel().equals("JH")){
+            return JHSystemUtil.isScreenAutoMode();
+        } else {
+            return LDSystemUtil.isScreenAutoMode();
+        }
+    }
+
+    /**
+     * 重启
+     */
+    @SuppressLint("WrongConstant")
+    public static void reboot() {
+        if(AppCommonConfig.getChannel().equals("JH")){
+            JHSystemUtil.reboot();
+        } else {
+            LDSystemUtil.reboot();
+        }
+    }
+
+    /**
+     * 关机
+     */
+    @SuppressLint("WrongConstant")
+    public static void shutdown() {
+        if(AppCommonConfig.getChannel().equals("JH")){
+            JHSystemUtil.shutdown();
+        } else {
+            LDSystemUtil.shutdown();
+        }
+    }
+
+    //断开以太网
+    @SuppressLint("WrongConstant")
+    public static void disconnectEth() {
+        if(AppCommonConfig.getChannel().equals("JH")){
+            JHSystemUtil.disconnectEth();
+        } else {
+            LDSystemUtil.disconnectEth();
+        }
+    }
+
+    //连接以太网
+    @SuppressLint("WrongConstant")
+    public static void connectEth() {
+        if(AppCommonConfig.getChannel().equals("JH")){
+            JHSystemUtil.connectEth();
+        } else {
+            LDSystemUtil.connectEth();
+        }
+    }
+
+    /**
+     * 调节亮度
+     * 亮度值（0~255）
+     */
+    @SuppressLint("WrongConstant")
+    public static void lightSet(int light) {
+        if(AppCommonConfig.getChannel().equals("JH")){
+            JHSystemUtil.lightSet(light);
+        } else {
+            LDSystemUtil.lightSet(light);
+        }
+    }
+
+    /**
+     * 开关背光
+     *
+     * @param value
+     */
+    @SuppressLint("WrongConstant")
+    public static void setLcdBlackLight(boolean value) {
+        if(AppCommonConfig.getChannel().equals("JH")){
+            JHSystemUtil.setLcdBlackLight(value);
+        } else {
+            LDSystemUtil.setLcdBlackLight(value);
+        }
+
+    }
+
+    /**
+     * 开关背光
+     *
+     * @param value
+     */
+    @SuppressLint("WrongConstant")
+    public static void openOrCloseScreen(boolean value) {
+        if(AppCommonConfig.getChannel().equals("JH")){
+            JHSystemUtil.openOrCloseScreen(value);
+        } else {
+            LDSystemUtil.openOrCloseScreen(value);
+        }
+    }
+
+    /**
+     * 隐藏导航栏
+     */
+    public static void hideNavigationBar(Activity activity) {
+        if(AppCommonConfig.getChannel().equals("JH")){
+            JHSystemUtil.hideNavigationBar(activity);
+        } else {
+            LDSystemUtil.hideNavigationBar(activity);
+        }
+    }
+
+    /**
+     * 显示导航栏
+     *
+     * @param context
+     */
+    public static void showNavigationBar(Context context) {
+        if(AppCommonConfig.getChannel().equals("JH")){
+            JHSystemUtil.showNavigationBar(context);
+        } else {
+            LDSystemUtil.showNavigationBar(context);
+        }
+    }
+
+    /**
+     * 继电器控制
+     */
+    @SuppressLint("WrongConstant")
+    public static void CommandGP(int num, boolean onoff) {
+        if(AppCommonConfig.getChannel().equals("JH")){
+            JHSystemUtil.CommandGP(num, onoff);
+        } else {
+            LDSystemUtil.CommandGP(num, onoff);
+        }
+    }
+
+    /**
+     * 继电器控制
+     */
+    @SuppressLint("WrongConstant")
+    public static Boolean readGP(int num) {
+        if(AppCommonConfig.getChannel().equals("JH")){
+            return JHSystemUtil.readGP(num).equals("1");
+        } else {
+            return LDSystemUtil.readGP(num).equals("1");
+        }
+    }
+
+    public static void clickScreen(int x,int y){
+        if(AppCommonConfig.getChannel().equals("JH")){
+             JHSystemUtil.clickScreen(x,y);
+        } else {
+             LDSystemUtil.clickScreen(x,y);
+        }
+
+    }
+
+    public static void wakeupScreenAndClickScreen(int x,int y){
+        if(AppCommonConfig.getChannel().equals("JH")){
+            JHSystemUtil.wakeupScreenAndClickScreen(x, y);
+        } else {
+            LDSystemUtil.wakeupScreenAndClickScreen(x, y);
+        }
+    }
+
+}
