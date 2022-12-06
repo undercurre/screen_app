85401f08868ceffc59c211eef9a2e7ec811ca34f
linhy29 <linhy29@midea.com>
2022-12-06 15:51:36 +0800

    feat: 凉霸控制逻辑完成

--- lib/routes/plugins/0x40/mode_list.dart
+++ lib/routes/plugins/0x40/mode_list.dart
@@ -6,6 +6,6 @@ var weak = Mode('weak', '弱风', 'assets/imgs/plugins/0x40/weak_wind_on.png', '
 
 var light = Mode('light', '照明', 'assets/imgs/plugins/0x40/light_on.png', 'assets/imgs/plugins/0x40/light_off.png');
 
-var refresh = Mode('refresh', '换气', 'assets/imgs/plugins/0x40/refresh_on.png', 'assets/imgs/plugins/0x40/refresh_off.png');
+var ventilation = Mode('ventilation', '换气', 'assets/imgs/plugins/0x40/ventilation_on.png', 'assets/imgs/plugins/0x40/ventilation_off.png');
 
-var coolMasterMode = <Mode>[strong, weak, light, refresh];
+var coolMasterMode = <Mode>[strong, weak, light, ventilation];
