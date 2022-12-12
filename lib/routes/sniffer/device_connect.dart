091cd5361a1fa66bc6e261eb2152ffcf29ee12d4
kevinsheep <yangws2@midea.com>
2022-12-12 18:16:54 +0800

    feat: 设备发现页完善

--- lib/routes/sniffer/device_connect.dart
+++ lib/routes/sniffer/device_connect.dart
@@ -1,11 +1,13 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:screen_app/widgets/index.dart';
-import '../../common/global.dart';
+
+var roomList = {'客厅': '客厅', '餐厅': '餐厅', '主卧': '主卧', '客卧': '客卧', '阳台': '阳台'};
 
 class DeviceConnectState extends State<DeviceConnectPage> {
   bool isLoading = true;
   late Timer _timer; // to be deleted
+  String selectedItem = roomList.keys.first;
 
   void goBack() {
     Navigator.pop(context);
@@ -39,6 +41,15 @@ class DeviceConnectState extends State<DeviceConnectPage> {
 
   @override
   Widget build(BuildContext context) {
+    ButtonStyle buttonStyle = TextButton.styleFrom(
+        backgroundColor: const Color.fromRGBO(43, 43, 43, 1),
+        shape: const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+        padding: const EdgeInsets.symmetric(vertical: 15));
+    ButtonStyle buttonStyleOn = TextButton.styleFrom(
+        backgroundColor: const Color.fromRGBO(38, 122, 255, 1),
+        shape: const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+        padding: const EdgeInsets.symmetric(vertical: 15));
+
     return Container(
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
@@ -65,31 +76,72 @@ class DeviceConnectState extends State<DeviceConnectPage> {
           ),
 
           Positioned(
-              top: 100,
+              top: 70,
               left: 0,
               width: MediaQuery.of(context).size.width,
               child: MzCell(
-                  title: "Cell",
-                  rightSlot: DropdownButton(
-                    value: "A",
-                    borderRadius: const BorderRadius.all(Radius.circular(10)),
-                    alignment: AlignmentDirectional.center,
-                    items: const [
-                      DropdownMenuItem(value: "A", child: Text("Item A")),
-                      DropdownMenuItem(value: "b", child: Text("Item B")),
-                    ],
-                    icon: Image.asset(
-                      "assets/imgs/icon/arrow-right.png",
-                      width: 30,
-                      height: 30,
-                    ),
-                    onChanged: (e) {
-                      logger.i('changed');
-                    },
-                  ))),
+                title: "智能窗帘",
+                titleSize: 24,
+                rightSlot: DropdownButtonHideUnderline(
+                    child: DropdownButton(
+                  value: selectedItem,
+                  borderRadius: const BorderRadius.all(Radius.circular(10)),
+                  alignment: AlignmentDirectional.topCenter,
+                  items: roomList.keys
+                      .map<DropdownMenuItem<String>>((String item) {
+                    return DropdownMenuItem<String>(
+                      value: item,
+                      child: Container(
+                        alignment: Alignment.center,
+                        constraints: const BoxConstraints(minWidth: 100),
+                        child: Text(item,
+                            style: const TextStyle(
+                                fontSize: 24,
+                                fontFamily: "MideaType",
+                                fontWeight: FontWeight.w400)),
+                      ),
+                    );
+                  }).toList(),
+                  dropdownColor: const Color(0XFF626262),
+                  focusColor: Colors.blue,
+                  icon: const Icon(Icons.arrow_forward_ios_sharp),
+                  iconEnabledColor: Colors.white,
+                  onChanged: (String? value) {
+                    setState(() => selectedItem = value!);
+                  },
+                )),
+                hasTopBorder: true,
+                hasBottomBorder: true,
+                padding:
+                    const EdgeInsets.symmetric(vertical: 17, horizontal: 26),
+              )),
 
-
-
+          Positioned(
+              left: 0,
+              bottom: 0,
+              width: MediaQuery.of(context).size.width,
+              child: Row(children: [
+                Expanded(
+                  child: TextButton(
+                      style: buttonStyle,
+                      onPressed: () {},
+                      child: const Text('上一步',
+                          style: TextStyle(
+                              fontSize: 24,
+                              color: Colors.white,
+                              fontFamily: 'MideaType'))),
+                ),
+                Expanded(
+                  child: TextButton(
+                      style: buttonStyleOn,
+                      onPressed: () {},
+                      child: const Text('完成添加',
+                          style: TextStyle(
+                              fontSize: 24,
+                              color: Colors.white,
+                              fontFamily: 'MideaType'))),
+                ),
+              ])),
         ],
       ),
     );
