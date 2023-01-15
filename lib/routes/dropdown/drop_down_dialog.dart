f8e9f5fcf3c5d3869f7eedc7f5de144715bac3aa
linhy29 <linhy29@midea.com>
2023-01-15 18:04:42 +0800

    fix: 修复字体不跟随滑动缩小

--- lib/routes/dropdown/drop_down_dialog.dart
+++ lib/routes/dropdown/drop_down_dialog.dart
@@ -1,7 +1,6 @@
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import '../../channel/index.dart';
 import '../../channel/models/music_state.dart';
