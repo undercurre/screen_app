e71d43fa6cf827521ebb012c18aa291c128ee438
kevinsheep <yangws2@midea.com>
2023-01-16 10:41:23 +0800

    fix: 增加标志，预防特定情况下，天气待机页多次弹出的问题

--- lib/routes/weather/index.dart
+++ lib/routes/weather/index.dart
@@ -186,6 +186,7 @@ class WeatherPageState extends State<WeatherPage> {
           ],
         ),
         onTap: () {
+          Provider.of<WeatherChangeNotifier>(context, listen: false).weatherPageActive = false;
           Navigator.of(context).pop();
         });
   }
