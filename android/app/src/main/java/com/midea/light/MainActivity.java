313981ce64410067198776ecaf75960489c371f1
杨云龙 <http://git.midea.com/yangyl19/smartscreen4.git>
2023-01-31 10:38:59 +0800

    [AI] 小美语音唤醒状态回调bug修复

--- android/app/src/main/java/com/midea/light/MainActivity.java
+++ android/app/src/main/java/com/midea/light/MainActivity.java
@@ -83,7 +83,6 @@ public class MainActivity extends FlutterActivity {
         });
         AiManager.getInstance().addWakUpStateCallBack(b -> {
             if (b) {
-                mChannels.aiMethodChannel.cMethodChannel.invokeMethod("aiWakeUpState",1);
                 isFlashMusic=false;
                 if (!isScreenOn()) {
                     sendKeyEvent(KeyEvent.KEYCODE_BACK);
@@ -91,13 +90,17 @@ public class MainActivity extends FlutterActivity {
                 if (isAiSleep) {
                     isAiSleep = false;
                     isMusicPlay = MusicManager.getInstance().isPaying();
+                    mChannels.aiMethodChannel.cMethodChannel.invokeMethod("aiWakeUpState",1);
+
                 }
                 if (MusicManager.getInstance().isPaying()) {
                     MusicManager.getInstance().pauseMusic();
                 }
             } else {
-                mChannels.aiMethodChannel.cMethodChannel.invokeMethod("aiWakeUpState",0);
-                isAiSleep = true;
+                if(isAiSleep==false){
+                    isAiSleep = true;
+                    mChannels.aiMethodChannel.cMethodChannel.invokeMethod("aiWakeUpState",0);
+                }
                 if (isMusicPlay||isFlashMusic) {
                     MusicManager.getInstance().startMusic();
                 }
