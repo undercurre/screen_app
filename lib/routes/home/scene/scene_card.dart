46cfb0793cd77c4943f0c95afce4ffcb1f4ea0d7
lirh42 <lirh42@midea.com>
2023-02-27 17:19:10 +0800

    fix(issue:【场景列表】扩大触控+长按、轻触冲突): 改动GestureDetector事件和位置

--- lib/routes/home/scene/scene_card.dart
+++ lib/routes/home/scene/scene_card.dart
@@ -12,8 +12,7 @@ class SceneCard extends StatefulWidget {
   final bool? power;
   final Function(Scene scene)? onClick;
 
-  const SceneCard(
-      {super.key, required this.scene, this.power, this.onClick});
+  const SceneCard({super.key, required this.scene, this.power, this.onClick});
 
   @override
   State<SceneCard> createState() => _SceneCardState();
@@ -30,7 +29,6 @@ class _SceneCardState extends State<SceneCard> {
   }
 
   execScene() async {
-    logger.i('执行自身');
     setState(() {
       selfPower = true;
     });
@@ -51,53 +49,54 @@ class _SceneCardState extends State<SceneCard> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(6),
-      child: Stack(
-        children: [
-          Positioned(
-            child: Image(image: AssetImage(scene.bg), width: 136.0),
-          ),
-          SizedBox(
-            width: 136,
-            height: 179,
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(10, 6, 10, 0),
-              child: Flex(
-                direction: Axis.vertical,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    scene.name,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 22.0,
-                      fontFamily: 'MideaType',
-                      fontWeight: FontWeight.normal,
-                      decoration: TextDecoration.none,
+      child: GestureDetector(
+        onTapUp: (e) {
+          if (widget.onClick != null) {
+            widget.onClick!(scene);
+          } else {
+            execScene();
+          }
+        },
+        child: Stack(
+          children: [
+            Positioned(
+              child: Image(image: AssetImage(scene.bg), width: 136.0),
+            ),
+            SizedBox(
+              width: 136,
+              height: 179,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(10, 6, 10, 0),
+                child: Flex(
+                  direction: Axis.vertical,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      scene.name,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 22.0,
+                        fontFamily: 'MideaType',
+                        fontWeight: FontWeight.normal,
+                        decoration: TextDecoration.none,
+                      ),
                     ),
-                  ),
-                  Listener(
-                    onPointerDown: (e) {
-                      if (widget.onClick != null) {
-                        widget.onClick!(scene);
-                      } else {
-                        execScene();
-                      }
-                    },
-                    child: Image(
-                      image: AssetImage((widget.power != null ? widget.power! : selfPower)
-                          ? 'assets/imgs/scene/choose.png'
-                          : scene.icon),
+                    Image(
+                      image: AssetImage(
+                          (widget.power != null ? widget.power! : selfPower)
+                              ? 'assets/imgs/scene/choose.png'
+                              : scene.icon),
                       width: 60.0,
                     ),
-                  )
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
