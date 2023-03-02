e2b1c2dc7c451636ddb085715f235fa708cd6118
杨云龙 <http://git.midea.com/yangyl19/smartscreen4.git>
2023-03-02 10:50:33 +0800

    [音乐] 修复播放音乐bug

--- lib/routes/sniffer/device_item.dart
+++ lib/routes/sniffer/device_item.dart
@@ -58,7 +58,7 @@ class DeviceItem extends StatelessWidget {
                       shape: BoxShape.circle,
                       border: device.selected
                           ? Border.all(
-                              strokeAlign: StrokeAlign.center,
+                              strokeAlign: BorderSide.strokeAlignCenter,
                               color: const Color.fromRGBO(0, 145, 255, 1),
                               width: 3,
                               style: BorderStyle.solid)
