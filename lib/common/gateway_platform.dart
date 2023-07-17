f2034800ea00b450aa1bd069008fa11c1d0aab98
weinp <effective_code@163.com>
2023-07-17 09:31:11 +0800

    [功能]临时提交切换平台逻辑

--- lib/common/gateway_platform.dart
+++ lib/common/gateway_platform.dart
@@ -18,11 +18,23 @@ enum GatewayPlatform {
 
 /// 网关环境切换通知
 class MideaRuntimePlatform extends ChangeNotifier {
-  GatewayPlatform platform = GatewayPlatform.NONE;
+  /// 默认为NONE
+  static GatewayPlatform platform = GatewayPlatform.NONE;
 
-  void setPlatform(GatewayPlatform platform) {
-    this.platform = platform;
-    notifyListeners();
+}
+
+/// 切换平台Helper
+/// 切换逻辑与初始化平台相关的统一写在此处
+class ChangePlatformHelper {
+
+  /// 切换到美居
+  static void changeToMeiju(BuildContext context) async {
+    MideaRuntimePlatform.platform = GatewayPlatform.MEIJU;
+  }
+
+  /// 切换到Homlux
+  static void changeToHomlux(BuildContext context) async {
+    MideaRuntimePlatform.platform = GatewayPlatform.HOMLUX;
   }
 
 }
