a6e85e16dc6947bec5958dfedb5de2801b8fbc06
weinp <effective_code@163.com>
2023-07-18 16:21:02 +0800

    [功能]兼容双平台方案

--- lib/common/gateway_platform.dart
+++ lib/common/gateway_platform.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:screen_app/channel/index.dart';
 import 'package:screen_app/common/homlux/api/homlux_api.dart';
 import 'package:screen_app/common/homlux/homlux_global.dart';
 import 'package:screen_app/common/meiju/api/meiju_api.dart';
@@ -32,21 +33,55 @@ class MideaRuntimePlatform extends ChangeNotifier {
 /// 切换逻辑与初始化平台相关的统一写在此处
 class ChangePlatformHelper {
 
-  /// 切换到美居
-  static void changeToMeiju(BuildContext context) async {
-    MideaRuntimePlatform.platform = GatewayPlatform.MEIJU;
-    HomluxGlobal.setLogout();
-    MeiJuApi.init();
-    System.initForMeiju();
+  /// 检查网关运行的环境
+  /// 返回值 0NONE 1美居 2美的照明
+  static Future<int> checkGatewayPlatform() async {
+    return gatewayChannel.checkGatewayPlatform();
+  }
 
+  /// 设置网关运行环境为美的照明，但子设备不会清空。为了兼容ota升级之后，恢复用户身份登录
+  Future<bool> setMeijuPlatFormFlag() async {
+    return gatewayChannel.setMeijuPlatFormFlag();
+  }
+
+
+  /// 切换到美居
+  /// gatewaySubDeviceDel 参数含义：是否要将网关子设备清空
+  static Future<bool> changeToMeiju(BuildContext context, [bool gatewaySubDeviceDel = true]) async {
+    if(gatewaySubDeviceDel) {
+      bool suc = await gatewayChannel.setMeijuPlatform();
+      if(suc) {
+        MideaRuntimePlatform.platform = GatewayPlatform.MEIJU;
+      }
+      return suc;
+    } else {
+      MideaRuntimePlatform.platform = GatewayPlatform.MEIJU;
+      HomluxGlobal.setLogout();
+      MeiJuApi.init();
+      System.initForMeiju();
+      return true;
+    }
   }
 
   /// 切换到Homlux
-  static void changeToHomlux(BuildContext context) async {
-    MideaRuntimePlatform.platform = GatewayPlatform.HOMLUX;
-    MeiJuGlobal.setLogout();
-    HomluxApi.init();
-    System.initForHomlux();
+  /// gatewaySubDeviceDel 参数含义：是否要将网关子设备清空
+  static Future<bool> changeToHomlux(BuildContext context, [bool gatewaySubDeviceDel = true]) async {
+    if(gatewaySubDeviceDel) {
+      bool suc = await gatewayChannel.setHomluxPlatForm();
+      if(suc) {
+        MideaRuntimePlatform.platform = GatewayPlatform.HOMLUX;
+        MeiJuGlobal.setLogout();
+        HomluxApi.init();
+        System.initForHomlux();
+      }
+      return suc;
+    } else {
+      MideaRuntimePlatform.platform = GatewayPlatform.HOMLUX;
+      MeiJuGlobal.setLogout();
+      HomluxApi.init();
+      System.initForHomlux();
+      return true;
+    }
 
   }
 
