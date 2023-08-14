433046a730ff8a2008624d805dc72311dba9259e
weinp <effective_code@163.com>
2023-08-14 18:21:59 +0800

    [功能]美居 - 更改网关云api名称

--- lib/common/meiju/api/meiju_gateway_cloud_api.dart
+++ lib/common/meiju/api/meiju_gateway_cloud_api.dart
@@ -4,7 +4,7 @@ import 'package:dio/dio.dart';
 import 'package:screen_app/common/meiju/api/meiju_api.dart';
 import 'package:screen_app/common/meiju/models/meiju_response_entity.dart';
 
-class MeiJuGatewayCloud {
+class MeiJuGatewayCloudApi {
   
   static Future<MeiJuResponseEntity> queryPanelBindList(String applianceCode) async {
     var res = await MeiJuApi.requestGCloudIot(
