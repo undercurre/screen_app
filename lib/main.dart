9a73fa45a7344307e85067071e14cdb74af496ea
weinp <effective_code@163.com>
2023-09-26 21:30:38 +0800

    [功能]解决已知问题

--- lib/main.dart
+++ lib/main.dart
@@ -70,8 +70,8 @@ class _App extends State<App> {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => DeviceListModel()),
-        ChangeNotifierProvider(create: (_) => RoomModel()),
+        // ChangeNotifierProvider(create: (_) => DeviceListModel()),
+        // ChangeNotifierProvider(create: (_) => RoomModel()),
         ChangeNotifierProvider(create: (_) => StandbyChangeNotifier()),
         ChangeNotifierProvider(create: (_) => GlobalRouteObserverNotifier()),
         ChangeNotifierProvider(create: (_) => LayoutModel()),
