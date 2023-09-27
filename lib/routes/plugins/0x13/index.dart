9d1e9714d5ec559eebbdfa0928eee2db472ac08c
liumn <mannan.liu@midea.com>
2023-09-27 18:02:54 +0800

    fix: 修复测试问题

--- lib/routes/plugins/0x13/index.dart
+++ lib/routes/plugins/0x13/index.dart
@@ -94,13 +94,14 @@ class WifiLightPageState extends State<WifiLightPage> with Throttle {
               child: LightBall(
                 brightness: formatValue(dataAdapter?.data!.brightness ?? 1),
                 colorTemperature: 100 - formatValue(dataAdapter?.data!.colorTemp ?? 0),
-              )),
+              )
+          ),
           Flex(
             direction: Axis.vertical,
             children: <Widget>[
               Container(
                 color: Colors.transparent,
-                margin: const EdgeInsets.fromLTRB(0, 0, 0, 35),
+                margin: const EdgeInsets.fromLTRB(0, 0, 0, 20),
                 child: ConstrainedBox(
                   constraints: const BoxConstraints(
                     minWidth: double.infinity,
@@ -121,138 +122,136 @@ class WifiLightPageState extends State<WifiLightPage> with Throttle {
                 flex: 1,
                 child: SizedBox(
                   width: MediaQuery.of(context).size.width,
-                  child: Center(
-                    child: EasyRefresh(
-                      header: const ClassicHeader(
-                        dragText: '下拉刷新',
-                        armedText: '释放执行刷新',
-                        readyText: '正在刷新...',
-                        processingText: '正在刷新...',
-                        processedText: '刷新完成',
-                        noMoreText: '没有更多信息',
-                        failedText: '失败',
-                        messageText: '上次更新 %T',
-                        mainAxisAlignment: MainAxisAlignment.end,
-                      ),
-                      onRefresh: () {
-                        dataAdapter?.fetchData();
-                      },
-                      child: SingleChildScrollView(
-                        child: Row(
-                          children: [
-                            const Align(
-                              widthFactor: 1,
-                              heightFactor: 2,
-                              alignment: Alignment(-1.0, -0.63),
-                              child: SizedBox(
-                                width: 152,
-                                height: 0,
-                              ),
+                  child: EasyRefresh(
+                    header: const ClassicHeader(
+                      dragText: '下拉刷新',
+                      armedText: '释放执行刷新',
+                      readyText: '正在刷新...',
+                      processingText: '正在刷新...',
+                      processedText: '刷新完成',
+                      noMoreText: '没有更多信息',
+                      failedText: '失败',
+                      messageText: '上次更新 %T',
+                      mainAxisAlignment: MainAxisAlignment.end,
+                    ),
+                    onRefresh: () {
+                      dataAdapter?.fetchData();
+                    },
+                    child: SingleChildScrollView(
+                      child: Row(
+                        children: [
+                          const Align(
+                            widthFactor: 1,
+                            heightFactor: 2,
+                            alignment: Alignment(-1.0, -0.63),
+                            child: SizedBox(
+                              width: 152,
+                              height: 0,
                             ),
-                            Expanded(
-                              child: Padding(
-                                padding: const EdgeInsets.fromLTRB(0, 0, 16, 0),
-                                child: ScrollConfiguration(
-                                  behavior: ScrollConfiguration.of(context)
-                                      .copyWith(scrollbars: false),
-                                  child: Column(
-                                    children: [
-                                      Container(
-                                        margin:
-                                            const EdgeInsets.only(bottom: 16),
-                                        child: ParamCard(
-                                          minValue: 1,
-                                          maxValue: 100,
-                                          title: '亮度',
-                                          disabled: dataAdapter?.data!.power ?? true ? false : true,
-                                          value: max(1, dataAdapter?.data!.brightness ?? 1),
-                                          activeColors: const [
-                                            Color(0xFFFFD185),
-                                            Color(0xFFFFD185)
-                                          ],
-                                          onChanged: dataAdapter?.controlBrightness,
-                                          onChanging: dataAdapter?.controlBrightness,
-                                        ),
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 0, 16, 0),
+                              child: ScrollConfiguration(
+                                behavior: ScrollConfiguration.of(context)
+                                    .copyWith(scrollbars: false),
+                                child: Column(
+                                  children: [
+                                    Container(
+                                      margin:
+                                      const EdgeInsets.only(bottom: 16),
+                                      child: ParamCard(
+                                        minValue: 1,
+                                        maxValue: 100,
+                                        title: '亮度',
+                                        disabled: dataAdapter?.data!.power ?? true ? false : true,
+                                        value: max(1, dataAdapter?.data!.brightness ?? 1),
+                                        activeColors: const [
+                                          Color(0xFFFFD185),
+                                          Color(0xFFFFD185)
+                                        ],
+                                        onChanged: dataAdapter?.controlBrightness,
+                                        onChanging: dataAdapter?.controlBrightness,
                                       ),
-                                      Container(
-                                        margin:
-                                            const EdgeInsets.only(bottom: 16),
-                                        child: ParamCard(
-                                          title: '色温',
-                                          unit: "K",
-                                          customMin: dataAdapter?.data?.minColorTemp ?? 2700,
-                                          customMax: dataAdapter?.data?.maxColorTemp ?? 6500,
-                                          disabled: dataAdapter?.data!.power ?? true ? false : true,
-                                          value: dataAdapter?.data!.colorTemp ?? 0,
-                                          activeColors: const [
-                                            Color(0xFFFFD39F),
-                                            Color(0xFF55A2FA)
-                                          ],
-                                          onChanged: dataAdapter?.controlColorTemperature,
-                                          onChanging: dataAdapter?.controlColorTemperature,
-                                        ),
+                                    ),
+                                    Container(
+                                      margin:
+                                      const EdgeInsets.only(bottom: 16),
+                                      child: ParamCard(
+                                        title: '色温',
+                                        unit: "K",
+                                        customMin: dataAdapter?.data?.minColorTemp ?? 2700,
+                                        customMax: dataAdapter?.data?.maxColorTemp ?? 6500,
+                                        disabled: dataAdapter?.data!.power ?? true ? false : true,
+                                        value: dataAdapter?.data!.colorTemp ?? 0,
+                                        activeColors: const [
+                                          Color(0xFFFFD39F),
+                                          Color(0xFF55A2FA)
+                                        ],
+                                        onChanged: dataAdapter?.controlColorTemperature,
+                                        onChanging: dataAdapter?.controlColorTemperature,
                                       ),
-                                      // if (((dataAdapter?.platform.inMeiju() ?? false)  && dataAdapter?.sn8 == '79009833')
-                                      //   || (dataAdapter?.platform.inHomlux() ?? false)) Container(
-                                      //   margin:
-                                      //       const EdgeInsets.only(bottom: 16),
-                                      //   child: ModeCard(
-                                      //     disabled: dataAdapter?.data!.power ?? true ? false : true,
-                                      //     modeList: lightModes,
-                                      //     selectedKeys: getSelectedKeys(),
-                                      //     onTap: (mode) {
-                                      //       setState(() {
-                                      //         modeTap = mode;
-                                      //       });
-                                      //       Timer(const Duration(milliseconds: 500), () {
-                                      //         setState(() {
-                                      //           modeTap = null;
-                                      //         });
-                                      //       });
-                                      //       dataAdapter?.controlMode(mode);
-                                      //     },
-                                      //   ),
-                                      // ),
-                                      // if ((dataAdapter?.platform.inMeiju() ?? false)  && dataAdapter?.sn8 == '79009833')
-                                      //   Container(
-                                      //   margin:
-                                      //       const EdgeInsets.only(bottom: 16),
-                                      //   child: FunctionCard(
-                                      //     title: '延时关灯',
-                                      //     subTitle: dataAdapter?.data!.timeOff == 0
-                                      //         ? '未设置'
-                                      //         : '${dataAdapter?.data!.timeOff}分钟后关灯',
-                                      //     child: Listener(
-                                      //       onPointerDown: (e) {
-                                      //         dataAdapter?.controlDelay();
-                                      //       },
-                                      //       child: Container(
-                                      //         width: 40,
-                                      //         height: 40,
-                                      //         decoration: BoxDecoration(
-                                      //           gradient: LinearGradient(
-                                      //             begin: Alignment.topRight,
-                                      //             end: Alignment.bottomLeft,
-                                      //             colors: dataAdapter?.data!.timeOff == 0 ? [const Color(0x21FFFFFF), const Color(0x21FFFFFF)]:
-                                      //               [const Color(0xFF767B86), const Color(0xFF88909F), const Color(0xFF516375)],
-                                      //           ),
-                                      //           borderRadius:
-                                      //               BorderRadius.circular(20),
-                                      //         ),
-                                      //         child: const Image(
-                                      //           image: AssetImage('assets/imgs/plugins/0x13/delay_off.png'),
-                                      //         ),
-                                      //       ),
-                                      //     ),
-                                      //   ),
-                                      // )
-                                    ],
-                                  ),
+                                    ),
+                                    // if (((dataAdapter?.platform.inMeiju() ?? false)  && dataAdapter?.sn8 == '79009833')
+                                    //   || (dataAdapter?.platform.inHomlux() ?? false)) Container(
+                                    //   margin:
+                                    //       const EdgeInsets.only(bottom: 16),
+                                    //   child: ModeCard(
+                                    //     disabled: dataAdapter?.data!.power ?? true ? false : true,
+                                    //     modeList: lightModes,
+                                    //     selectedKeys: getSelectedKeys(),
+                                    //     onTap: (mode) {
+                                    //       setState(() {
+                                    //         modeTap = mode;
+                                    //       });
+                                    //       Timer(const Duration(milliseconds: 500), () {
+                                    //         setState(() {
+                                    //           modeTap = null;
+                                    //         });
+                                    //       });
+                                    //       dataAdapter?.controlMode(mode);
+                                    //     },
+                                    //   ),
+                                    // ),
+                                    // if ((dataAdapter?.platform.inMeiju() ?? false)  && dataAdapter?.sn8 == '79009833')
+                                    //   Container(
+                                    //   margin:
+                                    //       const EdgeInsets.only(bottom: 16),
+                                    //   child: FunctionCard(
+                                    //     title: '延时关灯',
+                                    //     subTitle: dataAdapter?.data!.timeOff == 0
+                                    //         ? '未设置'
+                                    //         : '${dataAdapter?.data!.timeOff}分钟后关灯',
+                                    //     child: Listener(
+                                    //       onPointerDown: (e) {
+                                    //         dataAdapter?.controlDelay();
+                                    //       },
+                                    //       child: Container(
+                                    //         width: 40,
+                                    //         height: 40,
+                                    //         decoration: BoxDecoration(
+                                    //           gradient: LinearGradient(
+                                    //             begin: Alignment.topRight,
+                                    //             end: Alignment.bottomLeft,
+                                    //             colors: dataAdapter?.data!.timeOff == 0 ? [const Color(0x21FFFFFF), const Color(0x21FFFFFF)]:
+                                    //               [const Color(0xFF767B86), const Color(0xFF88909F), const Color(0xFF516375)],
+                                    //           ),
+                                    //           borderRadius:
+                                    //               BorderRadius.circular(20),
+                                    //         ),
+                                    //         child: const Image(
+                                    //           image: AssetImage('assets/imgs/plugins/0x13/delay_off.png'),
+                                    //         ),
+                                    //       ),
+                                    //     ),
+                                    //   ),
+                                    // )
+                                  ],
                                 ),
                               ),
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
