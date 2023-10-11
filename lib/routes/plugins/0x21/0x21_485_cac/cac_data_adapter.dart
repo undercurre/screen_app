8b0e5e9c6ec0944ae2b1d57a512779c332c855d9
yangyl19 <yangyl19@midea.com>
2023-10-11 15:58:51 +0800

    485设备在线状态bug修复

--- lib/routes/plugins/0x21/0x21_485_cac/cac_data_adapter.dart
+++ lib/routes/plugins/0x21/0x21_485_cac/cac_data_adapter.dart
@@ -5,8 +5,6 @@ import '../../../../channel/models/local_485_device_state.dart';
 import '../../../../common/adapter/device_card_data_adapter.dart';
 import '../../../../common/adapter/midea_data_adapter.dart';
 import '../../../../common/api/api.dart';
-import '../../../../common/homlux/homlux_global.dart';
-import '../../../../common/homlux/models/homlux_485_device_list_entity.dart';
 import '../../../../common/logcat_helper.dart';
 import '../../../../common/meiju/api/meiju_device_api.dart';
 import '../../../../common/meiju/models/meiju_response_entity.dart';
