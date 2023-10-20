7f6d0985aa3d01917a00a55010d09391f0f0fab1
lirh42 <undercurre@163.com>
2023-10-20 20:49:16 +0800

    fix: homlux不生成自己同时生成默认布局

--- lib/states/device_list_notifier.dart
+++ lib/states/device_list_notifier.dart
@@ -502,6 +502,102 @@ class DeviceInfoListModel extends ChangeNotifier {
       }
     }
 
+    if (transformList.isEmpty) {
+      List<Layout> defaultList = [
+        Layout(
+            'clock',
+            DeviceEntityTypeInP4.Clock,
+            CardType.Other,
+            0,
+            [1, 2, 5, 6],
+            DataInputCard(
+                name: '时钟',
+                applianceCode: 'clock',
+                roomName: '屏内',
+                isOnline: '',
+                type: 'clock',
+                masterId: '',
+                modelNumber: '',
+                onlineStatus: '1')),
+        Layout(
+            'weather',
+            DeviceEntityTypeInP4.Weather,
+            CardType.Other,
+            0,
+            [3, 4, 7, 8],
+            DataInputCard(
+                name: '天气',
+                applianceCode: 'weather',
+                roomName: '屏内',
+                isOnline: '',
+                type: 'weather',
+                masterId: '',
+                modelNumber: '',
+                onlineStatus: '1')),
+        Layout(
+            'localPanel1',
+            DeviceEntityTypeInP4.LocalPanel1,
+            CardType.Small,
+            0,
+            [9, 10],
+            DataInputCard(
+                name: '灯1',
+                applianceCode: 'localPanel1',
+                roomName: '屏内',
+                isOnline: '',
+                type: 'localPanel1',
+                masterId: '',
+                modelNumber: '',
+                onlineStatus: '1')),
+        Layout(
+            'localPanel2',
+            DeviceEntityTypeInP4.LocalPanel2,
+            CardType.Small,
+            0,
+            [11, 12],
+            DataInputCard(
+                name: '灯2',
+                applianceCode: 'localPanel2',
+                roomName: '屏内',
+                isOnline: '',
+                type: 'localPanel2',
+                masterId: '',
+                modelNumber: '',
+                onlineStatus: '1')),
+        Layout(
+            uuid.v4(),
+            DeviceEntityTypeInP4.DeviceNull,
+            CardType.Null,
+            0,
+            [13, 14],
+            DataInputCard(
+                name: '',
+                applianceCode: '',
+                roomName: '',
+                isOnline: '',
+                type: '',
+                masterId: '',
+                modelNumber: '',
+                onlineStatus: '')),
+        Layout(
+            uuid.v4(),
+            DeviceEntityTypeInP4.DeviceNull,
+            CardType.Null,
+            0,
+            [15, 16],
+            DataInputCard(
+                name: '',
+                applianceCode: '',
+                roomName: '',
+                isOnline: '',
+                type: '',
+                masterId: '',
+                modelNumber: '',
+                onlineStatus: ''))
+      ];
+      transformList.addAll(defaultList);
+    }
+
     // 轮询填充空缺
     int lastPageIndex = getMaxPageIndex(transformList);
     for (int l = 0; l <= lastPageIndex; l ++) {
