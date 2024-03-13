f58279dc2505c4eaaf94375209dffbcd339c0780
weinp <weinp1@midea.com>
2024-03-13 09:55:22 +0800

    [功能]组件Toast，修改限制的字符长度

--- lib/common/utils.dart
+++ lib/common/utils.dart
@@ -43,7 +43,7 @@ class TipsUtils {
   /// 轻提示弹窗
   static void toast({String content = '', int duration = 2000, EasyLoadingToastPosition? position}) {
     EasyLoading.showToast(
-        NameFormatter.formLimitString(content, 20, 15, 0),
+        NameFormatter.formLimitString(content, 25, 20, 0),
         duration: Duration(milliseconds: duration), toastPosition: position);
   }
 
