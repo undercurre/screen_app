684e491c94f2d8d1b70ab814df6495ac5576301c
weinp <weinp1@midea.com>
2024-07-08 10:51:28 +0800

    [功能]对AI增加日志记录

---
+++ android/module-ai/src/main/java/com/midea/AIFileLogRecord.kt
@@ -0,0 +1,71 @@
+package com.midea.light.homeos
+
+import android.util.Log
+import org.json.JSONObject
+import java.io.File
+import java.io.RandomAccessFile
+import java.text.SimpleDateFormat
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.TimeUnit
+
+val FindEvent = mutableMapOf<String, Long>(
+    Pair("start", 0L),
+    Pair("end", 0L),
+)
+
+val ConnectEvent = mutableMapOf<String, Long>(
+    Pair("lose", 0L),
+    Pair("connected", 0L),
+    Pair("count", 0L)
+)
+
+
+
+object AIFileLogRecord {
+
+    var randomAccessFile: RandomAccessFile? = null
+    var fileLimit = 30 * 1024 * 1024
+    var executorService: ScheduledExecutorService? = null
+    var sdf: SimpleDateFormat = SimpleDateFormat("MM-dd HH:mm:ss.SSS")
+
+
+    init {
+        // 初始化文件
+        var file = File("/data/data/com.midea.light/cache/AIMideaLog.txt")
+        var createFileSuc = true
+        if(!file.exists()) {
+            var dir = file.parentFile!!
+            if(!dir.exists()) {
+                if(dir.mkdirs() && file.createNewFile()) {
+                    Log.i(this.javaClass.simpleName, "创建文件成功")
+                } else {
+                    Log.i(this.javaClass.simpleName, "创建文件失败")
+                    createFileSuc = false
+                }
+            }
+        }
+        if(createFileSuc) {
+            randomAccessFile = RandomAccessFile(file, "rws")
+            if(randomAccessFile!!.length() > fileLimit) {
+                randomAccessFile!!.seek(0)
+            }
+            executorService =  Executors.newSingleThreadScheduledExecutor()
+        }
+    }
+
+    fun record(message: String) {
+        if (randomAccessFile == null || executorService == null) return
+
+        executorService!!.execute {
+            if (randomAccessFile!!.length() == 0L) {
+                randomAccessFile!!.seek(0L)
+            }
+            randomAccessFile!!.write("${sdf.format(System.currentTimeMillis())} $message\n".toByteArray(Charsets.UTF_8))
+            if (randomAccessFile!!.length() > fileLimit) {
+                randomAccessFile!!.seek(0)
+            }
+        }
+    }
+
+}
